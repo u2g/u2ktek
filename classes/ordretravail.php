22fd2dbb806dc3d3ce60802935be260419d17798
gilles U2 <gu2@harmaguedone.u2net>
2019-03-05 21:36:58 +0100

    Creation des classes

---
+++ classes/ordretravail.php
@@ -0,0 +1,16 @@
+<?php
+    class OdreTravail{
+        private $_numeroAppel;
+        private $_adresse;
+        private $_numeroContact;
+        private $_remarques;
+        private $_testBiq;
+        private $_emailClient;
+        private $_dateIntervention;
+
+        function __contruct(){
+
+        }
+
+    }
+?>
